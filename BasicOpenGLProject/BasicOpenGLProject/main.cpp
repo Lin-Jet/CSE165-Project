d133626c2293649c70110cdd2d4835593837485d
Jet Lin <jetlin101@gmail.com>
2024-04-27 15:04:16 -0700

    added borders, and turning back to brown, and counter in terminal

--- BasicOpenGLProject/BasicOpenGLProject/main.cpp
+++ BasicOpenGLProject/BasicOpenGLProject/main.cpp
@@ -4,6 +4,51 @@
 #include <cstdlib>
 #include <ctime>
 
+class Shape {
+public:
+    virtual void draw() = 0; // Pure virtual function to draw the shape
+};
+
+class Triangle : public Shape {
+private:
+    float x;
+    float y;
+public:
+    Triangle(float x, float y) : x(x), y(y) {}
+
+    void draw() override {
+        glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glVertex2f(x, y);
+        glVertex2f(x + 0.3f, y);
+        glVertex2f(x + 0.15f, y + 0.3f);
+        glEnd();
+    }
+};
+
+class Square : public Shape {
+private:
+    float x;
+    float y;
+    bool isRed;
+public:
+    Square(float x, float y, bool isRed) : x(x), y(y), isRed(isRed) {}
+
+    void draw() override {
+        glBegin(GL_QUADS);
+        if (isRed) {
+            glColor3f(1.0f, 0.0f, 0.0f); // Red color
+        } else {
+            glColor3f(0.6f, 0.3f, 0.0f); // Brown color
+        }
+        glVertex2f(x - 0.1f, y - 0.1f);
+        glVertex2f(x + 0.1f, y - 0.1f);
+        glVertex2f(x + 0.1f, y + 0.1f);
+        glVertex2f(x - 0.1f, y + 0.1f);
+        glEnd();
+    }
+};
+
 // Triangle position
 float triangleX = 0.0f;
 float triangleY = 0.0f;
@@ -25,11 +70,15 @@ bool keyW = false;
 bool keyA = false;
 bool keyS = false;
 bool keyD = false;
+bool keySpace = false;
 
 // Timer interval for changing square colors (in milliseconds)
 int timerInterval = 2000; // 2 seconds
 // Timer ID
 int timerID;
+
+// Counter for squares changed back to brown
+int brownCount = 0;
 
 // Check if a point (x, y) is inside the square at (sx, sy)
 bool isInsideSquare(float x, float y, float sx, float sy) {
@@ -70,6 +119,9 @@ void keyboard_func(unsigned char key, int x, int y) {
         case 'd':
             keyD = true;
             break;
+        case ' ':
+            keySpace = true;
+            break;
         case '\x1B': // Exit on escape key press
             exit(EXIT_SUCCESS);
             break;
@@ -89,6 +141,9 @@ void key_released(unsigned char key, int x, int y) {
             break;
         case 'd':
             keyD = false;
+            break;
+        case ' ':
+            keySpace = false;
             break;
     }
 }
@@ -116,10 +171,33 @@ void display_func(void) {
     }
 
     // Update triangle position based on key presses
-    if (keyW) triangleY += movementSpeed;
-    if (keyA) triangleX -= movementSpeed;
-    if (keyS) triangleY -= movementSpeed;
-    if (keyD) triangleX += movementSpeed;
+    if (keyW && triangleY + movementSpeed <= 0.8f) triangleY += movementSpeed;
+    if (keyA && triangleX - movementSpeed >= -1.3f) triangleX -= movementSpeed;
+    if (keyS && triangleY - movementSpeed >= -1.2f) triangleY -= movementSpeed;
+    if (keyD && triangleX + movementSpeed <= 0.8f) triangleX += movementSpeed;
+
+    if (keySpace) {
+        std::cout << "space pressed" << std::endl;
+        for (int i = 0; i < 6; ++i) {
+            if (isInsideSquare(triangleX, triangleY, squarePositions[i][0], squarePositions[i][1])) {
+                std::cout << "is in square: (" << squarePositions[i][0] << ", " <<  squarePositions[i][1] << ")" << std::endl;
+
+                drawSquare(squarePositions[i][0], squarePositions[i][1], false);
+                std::cout << "square color changed" << std::endl; 
+                brownCount++;
+                std::cout << "count: " << brownCount << std::endl; 
+    
+                // If the square is red, change its color to brown
+                if (squareColors[i]) {
+                    squareColors[i] = false;
+                    std::cout << "color changed" << std::endl;
+                    brownCount++; // Increment the counter
+                    std::cout << "count: " << brownCount << std::endl; 
+                    break; // Exit the loop after changing the color of the first touched square
+                }
+            }
+        }
+    }
 
     // Draw the triangle at its updated position
     glPushMatrix();
@@ -142,10 +220,8 @@ void init(void) {
     // Seed random number generator
     srand(time(NULL));
 
-	
-	// Start the timer for changing square colors
-	glutTimerFunc(timerInterval, changeSquareColors, 0);
-
+    // Start the timer for changing square colors
+    glutTimerFunc(timerInterval, changeSquareColors, 0);
 
     std::cout << "Finished initializing...\n\n";
 }
@@ -167,6 +243,8 @@ int main(int argc, char** argv) {
     init();
 
     glutMainLoop();
+
+    std::cout << "Squares changed back to brown: " << brownCount << std::endl; // Print the count when the program exits
 
     return EXIT_SUCCESS;
 }
